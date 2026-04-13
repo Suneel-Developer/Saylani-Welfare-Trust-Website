684236a172dea5b43c24ed9187a8d069415a39f9
Suneel Kumar <115105688+Suneel-Developer@users.noreply.github.com>
2026-04-14 04:14:52 +0500

    Refactor Contact component structure and iframe attributes

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -33,8 +33,7 @@ const Contact = () => {
           <div className="locationMap">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d904.86223363387!2d67.068066!3d24.882664!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33eea39ffafe1%3A0x66f953936852019b!2sSaylani%20Welfare%20Int.%20Trust!5e0!3m2!1sen!2sus!4v1681801766156!5m2!1sen!2sus"
-              
-             
+              title="Saylani Welfare Office Location"
               allowfullscreen=""
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
